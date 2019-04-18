992e7f46b4134c9778fa17cdd12ef9832ef2aaac
Michal Powaga <michalpowaga13@gmail.com>
2019-04-18 11:19:16 +0100

    Update readme and amounts in config.js

--- src/config.js
+++ src/config.js
@@ -12,8 +12,8 @@ export default {
     publicKey: 'ak_24RZkzfKAQCxMD66shsbEUVEbQ3inU45Gu3j3VCSgF7GwNbZaU',
     secretKey: 'd549faca2aa008dc0d5e99268c613d29c5a23c809795ca1b88d6a6e5fe716c778b783a6b93d1f1686dfaae3f25a539799246fe0e469a11250fba02c749055c08'
   },
-  // Deposit required to start the game (0.1 AE)
-  deposit: 100000000000000000,
-  // Reward for winning the game (0.005 AE)
-  reward:  5000000000000000
+  // Deposit required to start the game (1 AE)
+  deposit: 1000000000000000000,
+  // Reward for winning the game (0.05 AE)
+  reward:  50000000000000000
 }
